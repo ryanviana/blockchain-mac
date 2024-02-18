c53f17f28a97283b3554f2dad7046c72927a5e91
Ryan Viana <ryanviana@usp.br>
2024-02-18 16:27:18 -0300

    Introduz Config Files

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,28 +1,27 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+require("ethers");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  const Proposal = await ethers.deployContract("ProposalContract");
+  await Proposal.waitForDeployment();
+  const proposalAddress = Proposal.target;
+  const Payment = await ethers.deployContract("PaymentContract", [
+    proposalAddress,
+  ]);
+  await Payment.waitForDeployment();
+  const paymentAddress = Payment.target;
+  const AccessControl = await ethers.deployContract("MACAccessControl");
+  await AccessControl.waitForDeployment();
+  const accessControlAddress = AccessControl.target;
+  const MACPlatformManager = await ethers.deployContract("MACPlatformManager", [
+    proposalAddress,
+    paymentAddress,
+    accessControlAddress,
+  ]);
+  await MACPlatformManager.waitForDeployment();
+  console.log("Proposal deployed to:", proposalAddress);
+  console.log("Payment deployed to:", paymentAddress);
+  console.log("AccessControl deployed to:", accessControlAddress);
+  console.log("MACPlatformManager deployed to:", MACPlatformManager.target);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
